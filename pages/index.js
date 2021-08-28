7c02fc70c1c382cdc519beb5c2cea0aac19ec5b4
djjark <33824775+djjark@users.noreply.github.com>
2021-08-28 16:01:34 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
+  return (asdasdasd
     <div className="container">
       <Head>
         <title>Create Next App</title>
