99e0e52c1ba8e5be7f20dddb989ec516710b3f11
djjark <diogofcordeiro@gmail.com>
2021-08-28 16:09:51 +0100

    push 4

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>o octavio é gay</div>
+      <div>o octavio é gay 1</div>
       <a href="https://cdn.discordapp.com/attachments/810858598954958862/881173812916396052/23023608_1482711901764655_343044072_n.jpg">Click to get a surprise</a>
       <footer>
         <a
