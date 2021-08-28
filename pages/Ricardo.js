dc51e3c69cada39eb0f139930199b8a67117e6e8
djjark <diogofcordeiro@gmail.com>
2021-08-28 16:48:03 +0100

    Update Ricardo.js

---
+++ pages/Ricardo.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+export default function Ricardo() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    <header></header>
+      <div>o octavio é gay 1</div>
+      <a href="https://cdn.discordapp.com/attachments/810858598954958862/881173812916396052/23023608_1482711901764655_343044072_n.jpg">Click to get a surprise</a>
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className="logo" />
+        </a>
+      </footer>
+   </div>
+  )
+}
