1e2fdc7c2ffdbc312ed2bb1d4459fcf025e6aeaf
djjark <diogofcordeiro@gmail.com>
2021-08-29 14:30:12 +0100

    a1

--- components/Layout.js
+++ components/Layout.js
@@ -3,8 +3,6 @@ import React from 'react'
 const Layout = () => {
     return (
         <div className="butao1">
-            <a href="/Ricardo">Ricardo</a>
-            <a href="/Diogo">Diogo</a>
         </div>
     )
 }
