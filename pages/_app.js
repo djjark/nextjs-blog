6b4a79ce3716eb338599b65353f034294b2030a3
djjark <diogofcordeiro@gmail.com>
2021-08-29 13:17:32 +0100

    commit 29/08

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,14 @@ import "../css/global.css"
 import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Layout from "../components/Layout"
 export default function Home() {
     return (
-        <div className="container">
+        <div>
             <Navbar />
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <div className="diogo">o octavio é gay</div>
-            <a href="https://cdn.discordapp.com/attachments/810858598954958862/881173812916396052/23023608_1482711901764655_343044072_n.jpg">Click to get a surprise</a>
-
+            <div>
+                <Layout />
+            </div>
             <Footer />
         </div>
     )
