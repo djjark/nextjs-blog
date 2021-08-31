d772473b9f86aa4584a1ffc657cdfd3b85d48bb3
djjark <diogofcordeiro@gmail.com>
2021-08-31 16:14:22 +0100

    a65

--- pages/database/database.js
+++ pages/database/database.js
@@ -1,21 +1,21 @@
-import { MongoClient } from "mongodb";
+// import { MongoClient } from "mongodb";
 
-async function handler(req, res) {
-    if (req.method === 'POST') {
-        const data = req.body;
+// async function handler(req, res) {
+//     if (req.method === 'POST') {
+//         const data = req.body;
 
-        //const { title, image, address, description } = data;
-        const uri = "mongodb+srv://ricardo:luz@cluster0.knayb.mongodb.net/nodejs-blog?retryWrites=true&w=majority";
-        const client = await MongoClient.connect(uri);
-        const db = client.db();
+//         //const { title, image, address, description } = data;
+//         const uri = "mongodb+srv://ricardo:luz@cluster0.knayb.mongodb.net/nodejs-blog?retryWrites=true&w=majority";
+//         const client = await MongoClient.connect(uri);
+//         const db = client.db();
 
-        const blogCollection = db.collection('meetups');
-        const result = await blogCollection.insertOne(data);
+//         const blogCollection = db.collection('meetups');
+//         const result = await blogCollection.insertOne(data);
 
-        console.log(result);
+//         console.log(result);
 
-        client.close();
-        res.status(201).json({ message: 'diogo teste base de dados' });
-    }
-}
-export default handler;
+//         client.close();
+//         res.status(201).json({ message: 'diogo teste base de dados' });
+//     }
+// }
+// export default handler;
