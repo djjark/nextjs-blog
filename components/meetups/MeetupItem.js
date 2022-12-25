1374c4fa00665a0d1282ef1c5c6549e95d36b303
djjark <diogofcordeiro@gmail.com>
2022-12-25 18:52:57 +0000

    Update MeetupItem.js
    sd

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -4,6 +4,8 @@ import { useRouter } from 'next/router';
 import Image from 'next/image';
 import { useUser } from '@auth0/nextjs-auth0';
 
+
+ss
 function MeetupItem(props) {
   const router = useRouter();
   const { user, isLoading } = useUser()
